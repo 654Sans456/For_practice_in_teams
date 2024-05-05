f1a9628d7174c11d7256883c3977b71e870c1f96
654Sans456 <87007646+654Sans456@users.noreply.github.com>
2024-05-05 13:38:47 +0300

    attempt to add a client to the table (failed)

--- forms/guideForm/showCustomer.py
+++ forms/guideForm/showCustomer.py
@@ -1,10 +1,15 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 from showForms import guideForm, guideFormWindow
-
+#from Forms.guideForm.guideForm import *
 
 def customerOnClick():
     _translate = QtCore.QCoreApplication.translate
     guideForm.label.setText(_translate("guideForm", "Клиенты"))
+    
+    #rowPosition = Ui_guideDialog.tableWidget.rowCount()
+    
+    #Ui_guideDialog.tableWidget.insertRow(rowPosition)
+    #Ui_guideDialog.tableWidget.setItem(rowPosition, 0, QtGui.QTableWidgetItem("text1"))
 
     guideFormWindow.show()
 
